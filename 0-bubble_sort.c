783a9c1b1c04677c241e3a9ff38f5f85b7c94116
VikingVik123 <vodimmegwa@gmail.com>
2024-01-23 05:53:38 -0800

    Added files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,23 @@
-#include <stdio.h>
 #include "sort.h"
 
-/**
- * bubble_sort - Function name
- * @array: pointer to the array 2 sort
- * size: size of the array
- */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, k;
-	for (i = 0; i < size - 1; i++)
+	size_t i, j;
+	int tmp;
+
+	for (i = 0; i < size - 1; ++i)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i - 1; ++j)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j+1]);
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				
-				for (k = 0; k < size; k++)
-				{
-					if (k < size - 1)
-					{
-						_puts(", ");
-					}
-				}
-				_puts("\n");
+				print_array(array, size);
 			}
 		}
 	}
 }
+
