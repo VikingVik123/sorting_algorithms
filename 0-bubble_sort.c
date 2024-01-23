e1353f5bee6eb730835c15c17a3a4113fa2a3316
VikingVik123 <vodimmegwa@gmail.com>
2024-01-23 04:19:14 -0800

    Added files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - Function name
+ * @array: pointer to the array 2 sort
+ * size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j+1])
+			{
+				swap(&array[j], &array[j+1]);
+				
+				for (k = 0; k < size; k++)
+				{
+					printf("%d", array[k]);
+					if (k < size - 1)
+					{
+						printf(", ");
+					}
+				}
+				printf("\n");
+			}
+		}
+	}
+}
