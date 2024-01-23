b655b19ddbe6fe0c94831e1377a140324c2d006e
VikingVik123 <vodimmegwa@gmail.com>
2024-01-23 06:50:20 -0800

    Added files

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,49 +1,50 @@
 #include "sort.h"
 
-int lomuto_partition(int *array, size_t first, size_t last)
+/**
+ * lomuto_partition - Function that partitions an array
+ * @array: pointer to array to partition
+ * @low: minimum size of array
+ * @size: maximum size of array
+ */
+size_t lomuto_partition(int *array, size_t low, size_t high)
 {
-    int pivot = array[last];
-    size_t i = first;
-    size_t j;
+	int pivot = array[high];
+	size_t i = low - 1;
 
-    for (j = first; j < last; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            if (i != j)
-            {
-                swap(&array[i], &array[j]);
-                print_array(array, last - first + 1);
-            }
-            i++;
-        }
-    }
-
-    if (i != last)
-    {
-        swap(&array[i], &array[last]);
-        print_array(array, last - first + 1);
-    }
-
-    return i;
+	for (size_t j = low; j <= high - 1; ++j)
+	{
+		if (array[j] < pivot)
+		{
+			++i;
+			swap(&array[i], &array[j]);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	
+	return i + 1;
 }
 
 void quick_sort_recur(int *array, size_t first, size_t last, size_t size)
 {
-    if (first < last)
-    {
-        size_t pivot_index = lomuto_partition(array, first, last);
+	if (first < last)
+	{
+		size_t pivot_index = lomuto_partition(array, first, last);
 
-        if (pivot_index > 0)
-            quick_sort_recur(array, first, pivot_index - 1, size);
-        quick_sort_recur(array, pivot_index + 1, last, size);
-    }
+		if (pivot_index > 0)
+			quick_sort_recur(array, first, pivot_index - 1, size);
+		quick_sort_recur(array, pivot_index + 1, last, size);
+	}
 }
 
+/**
+ * quick_sort - function to sort array of integers
+ * @array: pointer to array of integers
+ * @size: size of the array
+ */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    quick_sort_recur(array, 0, size - 1, size);
+	quick_sort_recur(array, 0, size - 1, size);
 }
